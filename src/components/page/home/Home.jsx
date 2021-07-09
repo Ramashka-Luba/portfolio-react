4f5570149db90170a84ff0eb1410869b44bfae59
Ramashka Luba <lromashko911@gmail.com>
2021-07-09 23:37:48 +0300

    animation home components

--- src/components/page/home/Home.jsx
+++ src/components/page/home/Home.jsx
@@ -3,6 +3,17 @@ import TitleH2 from "../../common/titleH2/TitleH2";
 import TitleH3 from "../../common/titleH3/TitleH3";
 import s from "./Home.module.scss";
 import homePhoto from "./../../../assets/images/homePhoto.png";
+import decor1 from "./../../../assets/images/decor/decor1.png";
+import decor2 from "./../../../assets/images/decor/decor2.png";
+import decor3 from "./../../../assets/images/decor/decor3.png";
+import decor4 from "./../../../assets/images/decor/decor4.png";
+import decor5 from "./../../../assets/images/decor/decor5.png";
+import decor6 from "./../../../assets/images/decor/decor6.png";
+import decor7 from "./../../../assets/images/decor/decor7.png";
+import decor8 from "./../../../assets/images/decor/decor8.png";
+import decor9 from "./../../../assets/images/decor/decor9.png";
+import decor10 from "./../../../assets/images/decor/decor10.png";
+
 
 export const Home = () => {
     return (
@@ -10,14 +21,30 @@ export const Home = () => {
             <div className={s.wrapper}>
 
                 <div className={s.content}>
+                    <div className={s.decor}>
+                        <img className={s.shape1} src={decor1} alt="shape"/> 
+                        <img className={s.shape2} src={decor2} alt="shape"/>
+                        <img className={s.shape1} src={decor3} alt="shape"/>
+                        <img className={s.shape2} src={decor4} alt="shape"/>
+                        <img className={s.shape1} src={decor5} alt="shape"/>
+                    </div>
+                    
                     <div className={s.contentText}>
                         <div className={s.titleH3}><TitleH3 value="Hello, I’m" /></div>
                         <div className={s.name} >LUBA RAMASHKA</div>
                         <div className={s.titleH2}><TitleH2 value="HTML Developer" /></div>
                         <div className={s.btn}> <Button value="DOWNLOAD CV" /></div>
                     </div>
-                </div>
 
+                    <div className={s.decor}>
+                        <img className={s.shape2} src={decor6} alt="shape"/> 
+                        <img className={s.shape1} src={decor7} alt="shape"/>
+                        <img className={s.shape2} src={decor8} alt="shape"/>
+                        <img className={s.shape1} src={decor9} alt="shape"/>
+                        <img className={s.shape2} src={decor10} alt="shape"/>
+                    </div>
+                </div>
+                    
                 <div className={s.photo}>
                     <img className={s.img} src={homePhoto} alt="photo" />
                 </div>
