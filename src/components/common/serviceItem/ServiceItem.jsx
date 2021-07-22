dabc2ad39aaf01f71f7e0bba36b25e65a7118e42
Ramashka Luba <lromashko911@gmail.com>
2021-07-22 12:50:33 +0300

    responsive page Sarvice

--- src/components/common/serviceItem/ServiceItem.jsx
+++ src/components/common/serviceItem/ServiceItem.jsx
@@ -7,7 +7,7 @@ import s from "./ServiceItem.module.scss";
 
 export const ServiceItem= ( props ) => {
     return (
-        <div className={s.serviceItem}>
+        <div className={s.serviceItem} style={props.style}>
             <img className={s.icon} src={props.src} alt={props.alt}/>
             <p className={s.text}>{props.text}</p>
         </div>
--- src/components/page/service/Service.jsx
+++ src/components/page/service/Service.jsx
@@ -13,33 +13,45 @@ export const Service = () => {
     return (
         <section className={s.service}>
             <div className={s.blockTop}>
-                <TitleH2 value="Services"/>
-                <div className={s.titleH3}>
-                    <TitleH3 value="What I can do for you"/>
-                </div>
-                <div className={s.serviceContainer}>
-                    <div className={s.content}>
-                        <ServiceItem
-                        src={ReactIcons}
-                        alt="react icons"
-                        text="I code sites and applications on React of any complexity (Node.js, NPM, Yarn, plus minimal logic logic in Java Script)."
-                        />
-                        <ServiceItem
-                        src={CodeIcons}
-                        alt="code icons"
-                        text="Adaptive, cross-browser, simantic, valid layout, layout of email newsletters, Mobile first, Pixel-perfect, animation creation."
-                        />
-                        <ServiceItem
-                        src={DesktopIcon}
-                        alt="desktop icon"
-                        text="I own the graphic editors Figma, Photoshop, I have the practice and skill of creating websites, landing pages and mobile and web applications"
-                        />
+                <div className={s.wrapper}>
+                    <div className={s.titleH2}>
+                        <TitleH2 value="Services" />
+                    </div>
+                    <TitleH3 value="What I can do for you" />
+                    <div className={s.serviceContainer}>
+                        <div className={s.content}>
+                            <div className={s.seviceItem}>
+                                <ServiceItem
+                                    src={ReactIcons}
+                                    alt="react icons"
+                                    text="I code sites and applications on React of any complexity (Node.js, NPM, Yarn, plus minimal logic logic in Java Script)."
+                                />
+                            </div>
+                            <div className={s.seviceItem}>
+                                <ServiceItem
+                                    src={CodeIcons}
+                                    alt="code icons"
+                                    text="Adaptive, cross-browser, simantic, valid layout, layout of email newsletters, Mobile first, Pixel-perfect, animation creation."
+                                />
+                            </div>
+                            <div className={s.seviceItem}>
+                                <ServiceItem
+                                    src={DesktopIcon}
+                                    alt="desktop icon"
+                                    text="I own the graphic editors Figma, Photoshop, I have the practice and skill of creating websites, landing pages and mobile and web applications"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
             <div className={s.blockBotton}>
-                <TitleH3 value="Considering options for remote work and work in the office"/>
-                <Button value="WRITE ME"/>
+                <div className={s.titleH3}>
+                    <TitleH3 value="Considering options for remote work and work in the office" />
+                </div>
+                <Button value="WRITE ME"
+                />
             </div>
         </section>
     );
