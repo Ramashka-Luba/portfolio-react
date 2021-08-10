77ac732dc04d9c4c42bddcab46d73d517996890a
PavelMalta <shamashov87@mail.ru>
2021-08-10 17:59:11 +0300

    add formik and axios library, connected form for formic, customized request

--- src/components/common/input/Input.jsx
+++ src/components/common/input/Input.jsx
@@ -4,7 +4,7 @@ const Input = (props) => {
     return (
         <label>
             <span className={s.label}>{props.label}</span>
-            <input className={s.input} type={props.type} name={props.name}/>
+            <input className={s.input} type={props.type} name={props.name} {...props.formik.getFieldProps(props.name)}/>
         </label>
     );
 }
